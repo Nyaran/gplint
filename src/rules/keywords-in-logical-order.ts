5c5f04bf6ad23cb4b1817f6280b459b6b1616dac
AlexisArteaga13 <alexisarteagacarhua@gmail.com>
2023-07-15 03:17:58 -0500

    feat(*) update rule keywords in logical order

--- src/rules/keywords-in-logical-order.ts
+++ src/rules/keywords-in-logical-order.ts
@@ -1,22 +1,29 @@
 import * as gherkinUtils from './utils/gherkin';
-import {GherkinData, RuleError} from '../types';
-import {Step} from '@cucumber/messages';
-
+import _ from 'lodash';
+import { GherkinData, RuleSubConfig, RuleError } from '../types';
+import { Step, Scenario } from '@cucumber/messages';
 export const name = 'keywords-in-logical-order';
+export const availableConfigs = {
+  'detectMissingKeywords': false
+};
 
-export function run({feature}: GherkinData): RuleError[] {
+export function run({ feature }: GherkinData, configuration: RuleSubConfig<typeof availableConfigs>): RuleError[] {
   if (!feature) {
     return [];
   }
-
+  const mergedConfiguration = _.merge({}, availableConfigs, configuration);
+  const detectMissingKeywords = mergedConfiguration.detectMissingKeywords;
   const errors = [] as RuleError[];
-
   feature.children.forEach((child) => {
     const node = child.background || child.scenario;
     const keywordList = ['given', 'when', 'then'];
 
     let maxKeywordPosition = undefined as number;
-
+    const existsKeyword: { [key: string]: boolean } = {
+      given: false,
+      when: false,
+      then: false
+    };
     node.steps.forEach((step) => {
       const keyword = gherkinUtils.getLanguageInsensitiveKeyword(
         step,
@@ -31,28 +38,44 @@ export function run({feature}: GherkinData): RuleError[] {
 
       if (keywordPosition < maxKeywordPosition) {
         const maxKeyword = keywordList[maxKeywordPosition];
-        // console.log(createError(step, maxKeyword));
-        errors.push(createError(step, maxKeyword));
+        errors.push(createError(step, maxKeyword, null));
       }
-
+      existsKeyword[keywordList[keywordPosition]] = true;
       maxKeywordPosition =
         Math.max(maxKeywordPosition, keywordPosition) || keywordPosition;
     });
+    if (detectMissingKeywords && child.scenario && !Object.values(existsKeyword).every((value) => value === true)) {
+      const keys: string[] = [];
+      Object.entries(existsKeyword).forEach(([key, value]) => {
+        if (!value) {
+          keys.push(key);
+        }
+      });
+      errors.push(createError(null, keys.join(', '), child.scenario));
+    }
   });
 
   return errors;
 }
 
-function createError(step: Step, maxKeyword: string) {
-  return {
-    message:
-      'Step "' +
+function createError(step: Step, keyword: string, scenario: Scenario) {
+  let message, node;
+  if (scenario) {
+    message = 'The scenario "' + scenario.name + '" does not have the following keywords: ' + keyword;
+    node=scenario;
+  } else if (step) {
+    message = 'Step "' +
       step.keyword +
       step.text +
       '" should not appear after step using keyword ' +
-      maxKeyword,
+      keyword;
+    node = step;
+  }
+
+  return {
+    message,
     rule: name,
-    line: step.location.line,
-    column: step.location.column,
+    line: node.location.line,
+    column: node.location.column,
   };
 }
--- test/rules/no-structure-correct/NoViolationsWithDetectMissingKeywords.feature
+++
@@ -1,8 +0,0 @@
-Feature: Structure Correct - With Detect Missing Keyword
-
-    Scenario: Order correct
-        Given the step given
-        And the step order correct
-        When the step when
-        And the step order correct
-        Then the step then order correct
