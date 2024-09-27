a3f6362910992f37cde62f28bc3361baa7b304c3
Lucas Aymon <32235434+lucas-it@users.noreply.github.com>
2024-09-27 14:41:40 +0200

    feat: auto fix trailing spaces

--- src/rules/no-trailing-spaces.ts
+++ src/rules/no-trailing-spaces.ts
@@ -1,21 +1,30 @@
 import {GherkinData, RuleError} from '../types.js';
+import { getLineContent, modifyLine } from './utils/line.js';
 
 export const name = 'no-trailing-spaces';
 
-export function run({file}: GherkinData): RuleError[] {
+export function run({file}: GherkinData, _: unknown, autoFix: boolean): RuleError[] {
 	const errors = [] as RuleError[];
 	let lineNo = 1;
 	file.lines.forEach(line => {
 		if (/[\t ]+$/.test(line)) {
-			errors.push({message: 'Trailing spaces are not allowed',
-				rule   : name,
-				line   : lineNo,
-				column : 0,
-			});
+			if (autoFix) {
+				fix(lineNo);
+			} else {
+				errors.push({message: 'Trailing spaces are not allowed',
+					rule   : name,
+					line   : lineNo,
+					column : 0,
+				});
+			}
 		}
 
 		lineNo++;
 	});
+
+	function fix(line: number) {
+		modifyLine(file, line, getLineContent(file, line).replaceAll(/\s+$/g, ''));
+	}
 
 	return errors;
 }
