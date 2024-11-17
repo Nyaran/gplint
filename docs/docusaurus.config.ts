426369038782f7e0c07b4b3ba40467e9072cdb41
Luis Zurro de Cos <luiszurrodecos@gmail.com>
2024-11-17 22:35:53 +0100

    Fix: Fix github config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -5,135 +5,140 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'GPLint',
-  tagline: 'A Gherkin/Pickle linter (validator) written in JavaScript, and usable from command line.',
-  favicon: 'img/favicon.ico',
+	title: 'GPLint',
+	tagline: 'A Gherkin/Pickle linter (validator) written in JavaScript, and usable from command line.',
+	favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
+	// Set the production url of your site here
 	url: 'https://nyaran.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/gplint/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Nyaran', // Usually your GitHub org/user name.
-  projectName: 'GPLint', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'nyaran', // Usually your GitHub org/user name.
+	projectName: 'gplint', // Usually your repo name.
+	deploymentBranch: 'gh-pages',
 
-  trailingSlash: false,
+	trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			{
+				docs: {
+					sidebarPath: './sidebars.ts',
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ['rss', 'atom'],
+						xslt: true,
+					},
+					// Useful options to enforce blogging best practices
+					onInlineTags: 'warn',
+					onInlineAuthors: 'warn',
+					onUntruncatedBlogPosts: 'warn',
+				},
+				theme: {
+					customCss: './src/css/custom.css',
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    navbar: {
-      title: 'GPLint',
-      logo: {
-        alt: 'GPLint Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Docs',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/Nyaran/gplint',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting started',
-              to: '/docs/getting-started',
-            },
-            {
-              label: 'Rules',
-              to: '/docs/category/rules',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/gplint',
-            },
-            {
-              label: 'Issue Tracker',
-              href: 'https://github.com/Nyaran/gplint/issues',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/Nyaran/gplint',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} GPLint, Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		navbar: {
+			title: 'GPLint',
+			logo: {
+				alt: 'GPLint Logo',
+				src: 'img/logo.svg',
+			},
+			items: [
+				{
+					type: 'docSidebar',
+					sidebarId: 'docsSidebar',
+					position: 'left',
+					label: 'Docs',
+				},
+				{
+					to: '/blog',
+					label: 'Blog',
+					position: 'left',
+				},
+				{
+					href: 'https://github.com/Nyaran/gplint',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
+		},
+		footer: {
+			style: 'dark',
+			links: [
+				{
+					title: 'Docs',
+					items: [
+						{
+							label: 'Getting started',
+							to: '/docs/getting-started',
+						},
+						{
+							label: 'Rules',
+							to: '/docs/category/rules',
+						},
+					],
+				},
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'Stack Overflow',
+							href: 'https://stackoverflow.com/questions/tagged/gplint',
+						},
+						{
+							label: 'Issue Tracker',
+							href: 'https://github.com/Nyaran/gplint/issues',
+						},
+					],
+				},
+				{
+					title: 'More',
+					items: [
+						{
+							label: 'Blog',
+							to: '/blog',
+						},
+						{
+							label: 'GitHub',
+							href: 'https://github.com/Nyaran/gplint',
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} GPLint, Inc. Built with Docusaurus.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 
-  plugins: [
-	  require.resolve('docusaurus-lunr-search')
-  ],
+	plugins: [
+		require.resolve('docusaurus-lunr-search'),
+	],
 };
 
 export default config;
